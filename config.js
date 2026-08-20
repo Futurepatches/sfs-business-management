912d058abf0b6907b5bc2707a1ad09509a0745dd
Futurepatches <syedfarrukhmir@gmail.com>
2026-08-20 12:49:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Put your Apps Script /exec URL here ONCE. This is not a login credential.
 // After this, users only enter Username + Password.
 window.SFS_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbxNCJao3uQlKJgFsiKluf-LzAoeWacz_uhFkB4E67egKW0qiVRNx073zMYv4oKY_u-vLA/exec"
 };
