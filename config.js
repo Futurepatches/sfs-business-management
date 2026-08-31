e2e7e9d7d077504baf4dc38da5380d4d30b4c80d
Futurepatches <syedfarrukhmir@gmail.com>
2026-08-31 15:29:42 +0500

    Change API_URL in config.js
    Updated API_URL to new Google Apps Script Web App URL.

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Put the SAME Google Apps Script Web App /exec URL used by your current
 // working database deployment here. This is not shown on the login screen.
 window.SFS_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxNCJao3uQlKJgFsiKluf-LzAoeWacz_uhFkB4E67egKW0qiVRNx073zMYv4oKY_u-vLA/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwL0yc-sfBcxnyuD1eYk4AlYY1xEoyMjjFLLAP_7XQPnVud-Otyndoj46ydPVYSKE02OQ/exec"
 };
